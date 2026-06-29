a7ebd6271894655221df0b966afa70663af8e8db
shoryan5135s <shoryanagarwal154@gmail.com>
2026-06-29 23:55:48 +0530

    fixing email with brevo

--- backend/server/src/utils/emailService.js
+++ backend/server/src/utils/emailService.js
@@ -5,20 +5,28 @@ dotenv.config();
 const nodemailer=require('nodemailer')
 
 const transporter=nodemailer.createTransport({
-    service:"gmail",
-     connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-    auth:{
+     host:process.env.EMAIL_HOST,
+     port:Number(process.env.EMAIL_PORT),
+     secure:false,
+     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASSWORD
-    }
+     }
+    
 })
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log("SMTP Verify Error:", error);
+    } else {
+        console.log("SMTP Server is ready");
+    }
+});
 
 const sendEmail=async({to,subject,text,html,attachments})=>{
     try{
         const response=await transporter.sendMail({
-        from: process.env.EMAIL_USER,
+        from: process.env.EMAIL_FROM,
         to,
         subject,
         text,
